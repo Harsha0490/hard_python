8920f2f9ba489d614c23cd8902f6de91a06384e6
rshatkin <rachel.shatkin@RachelShatkin-mba.local>
2015-10-14 10:31:31 -0400

    pep8-ing my test

--- ex26.py
+++ ex26.py
@@ -1,34 +1,41 @@
 # import ex26
 
+
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
     return words
 
+
 def sort_words(words):
     """Sorts the words."""
     return sorted(words)
+
 
 def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print word
 
+
 def print_last_word(words):
     """Prints the last word after popping it off."""
     word = words.pop(-1)
     print word
+
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
     return sort_words(words)
 
+
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
     print_last_word(words)
+
 
 def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one."""
@@ -38,7 +45,9 @@ def print_first_and_last_sorted(sentence):
 
 
 print "Let's practice everything."
-print 'You\'d need to know \'bout escapes with \\ that do \nnewlines and \t tabs.'
+print 'You\'d need to know \'bout escapes with \\ that do " +
+"\nnewlines and \t tabs.'
+
 
 poem = """
 \tThe lovely world
@@ -57,6 +66,7 @@ print "--------------"
 five = 10 - 2 + 3 - 6
 print "This should be five: %s" % five
 
+
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
@@ -68,12 +78,14 @@ start_point = 10000
 jelly_beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
-print "We'd have %d beans, %d jars, and %d crates." % (jelly_beans, jars, crates)
+print "We'd have %d beans, %d jars, and %d crates." % (
+    jelly_beans, jars, crates)
 
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crates." %
+secret_formula(start_point)
 
 
 sentence = "All good things come to those who wait."
