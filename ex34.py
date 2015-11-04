7e8b5112db469fb23e51a3e5600a253e750bbeba
rshatkin <shatkin.r@gmail.com>
2015-11-04 09:31:02 -0500

    ex 34, 35, and start of 36

---
+++ ex34.py
@@ -0,0 +1,28 @@
+# ordinal numbers - indicate order of things. starts at 1.
+# cardinal numbers - starts at zero (index). You can pick at random,
+# so there needs to be a 0 element
+# More like a deck of cards. Can pick any card at any point.
+
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals[1]
+print animals[2]
+print animals[0]
+print animals[3]
+print animals[4]
+print animals[2]
+print animals[5]
+print animals[4]
+
+
+# the animal at 1 is the 2nd animal and is a python
+# the third animal is at 2 and is a peacock
+# the first animal is at 0 and is a bear
+# the the animal at 3 is the 4th animal and is a kangaroo
+# the fifth animal is at 4 and is a whale
+# the animal at 2 is the third animal and is a peacock
+# the sixth animal is at 5 and is a platypus
+# the animal at 4 is the 5th animal and is a whale
+
+
---
+++ ex36.py
@@ -0,0 +1,23 @@
+# use lists, functions, and modules (refer to ex13 if need be)
+# draw a map for the game
+# code up map
+
+# Should I WFH Today?
+
+# Are you in tech?
+# If 'No' end the game here and say
+# "Probably won't fly, unless you're dying. better go in"
+
+# Manager/Strict Mode?
+
+# Do you live in NYC? If yes add these questions:
+# Has there been an "Earlier Incident" on the train?
+# if NYC and Winter: How much walking in commute? Below 30 degrees?
+
+# What Month is it?
+# if winter time, ask about sniffles, snow fall, temp.
+#
+# if summer time, ask about summer friday, meetings, etc.
+
+#
+
